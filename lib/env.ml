047cf775e2e5fd09793aba9d73c7b9d503dd1806
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2024-10-21 21:19:23 +0200

    refactor env

--- lib/env.ml
+++ lib/env.ml
@@ -1,10 +1,12 @@
+open Core
+
 module Env = struct
   exception EnvVarNotFound of string
 
   let parse () = Dotenv.export ()
 
   let get_env_var var_name =
-    match Sys.getenv_opt var_name with
+    match Sys.getenv var_name with
     | Some var -> var
     | None ->
       let msg = Printf.sprintf "Environment variable '%s' not found" var_name in
